61e4a4a5fc7121adf349f23c16dc2643021a832c
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-06-11 14:21:12 -0400

    Connect state to input field

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, onSearchCategory }) {
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input type="text"
+      name="search"
+      onChange={onSearchCategory}
+      placeholder="Search..." />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,6 +5,12 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchCategory, setSearchCategory] = useState('')
+
+  function handleSearchChange(event) {
+    console.log(event.target.value)
+    setSearchCategory(event.target.value)
+  }
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
@@ -19,7 +25,7 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchCategory={handleSearchChange}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
